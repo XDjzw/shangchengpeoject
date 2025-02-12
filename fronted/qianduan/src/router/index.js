796aa63ea57e308352d4ed8bef3903f5b449c803
JiangZhiwei <3486983656@qq.com>
2025-02-12 17:55:54 +0800

    save

---
+++ fronted/qianduan/src/router/index.js
@@ -0,0 +1,38 @@
+// src/router/index.js
+
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue'; // 假设这是你的主页组件
+import Shopping from '../views/Shopping.vue'; // 购物页面组件
+import Live from '../views/Live.vue'; // 直播页面组件
+import My from '../views/My.vue'; // 我的账户页面组件
+
+const base =import.meta.env.VITE_BASE_URL || '/'
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/shopping',
+    name: 'Shopping',
+    component: Shopping
+  },
+  {
+    path: '/live',
+    name: 'Live',
+    component: Live
+  },
+  {
+    path: '/my',
+    name: 'My',
+    component: My
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(base),
+  routes
+});
+
+export default router;
---
+++ fronted/qianduan/src/views/shopping.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>购物</div>
+</template>
